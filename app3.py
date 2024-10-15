e64e468470bf6db0bd0b3ca115a624b915680117
actingcat <ava.davis.dupree@gmail.com>
2024-10-15 19:40:29 -0400

    -m newest app

---
+++ app3.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from flask_cors import CORS
+app = Flask(__name__)
+
+CORS(app)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['proj3test2']
+collection = db['collection_all']
+
+@app.route('/dataall', methods=['GET'])
+def fetch_data():
+    try:
+        data = list(collection.find())
+        for item in data:
+            item['_id'] = str(item['_id']) 
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
